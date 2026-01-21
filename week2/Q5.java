d8006d39e27e9cbc3967c53ee68e0192907727c3
AmanShakya0018 <amanshakya1808@gmail.com>
2026-01-21 11:26:14 +0530

    added 1526

--- week2/Q5.java
+++ week2/Q5.java
@@ -1,9 +1,24 @@
 
 // Question: https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/description/
-// My Sollution: 
-//My Approach: 
-//
-// TC: O()
+// lc - 1526
 public class Q5 {
 
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(Solution.minNumberOperations(new int[]{1, 2, 3, 2, 1}));
+
+    }
+
+}
+
+class Solution {
+
+    public static int minNumberOperations(int[] target) {
+        int n = target.length;
+        int count = target[0];
+        for (int i = 1; i < n; i++) {
+            count += Math.max(target[i] - target[i - 1], 0);
+        }
+        return count;
+    }
 }
